e61cbe63ddb7ad3e01d3c081fcf5a87944dc8b33
Joshua Gang <joshua.gang@gmail.com>
2018-07-26 23:37:49 -0400

    pep8

--- generate_deck_page.py
+++ generate_deck_page.py
@@ -16,6 +16,7 @@ if exists("card_backup.pkl"):
 else:
     memoizer = {}
 
+
 def return_url_line_type(cardName):
     quantity, name = cardName.split(" ", 1)
     quantity = quantity.strip()
@@ -32,7 +33,8 @@ def return_url_line_type(cardName):
             card = next(cards)
     except:
         return cardName, "Unknown", name
-    url = "http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid={}".format(card.multiverse_id)
+    url = "http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid={}".format(
+        card.multiverse_id)
     if "Land" in card.type:
         card_type = "Land"
     elif "Creature" in card.type:
@@ -51,54 +53,62 @@ def return_url_line_type(cardName):
         card_type = "Unknown"
     memoizer[name] = "[{}]({})".format(name, url), card_type, name
     return "{} [{}]({})".format(quantity, name, url), card_type, name
-    
+
+
 def get_links_group_by_types(input):
     results = []
     for cur in input:
         results.append(return_url_line_type(cur))
-    results = sorted(results, key = lambda x : (x[1], x[2]))
-    results = {k: list([y[0] for y in v]) for k, v in groupby(results, key=lambda x: x[1])}
+    results = sorted(results, key=lambda x: (x[1], x[2]))
+    results = {k: list([y[0] for y in v])
+               for k, v in groupby(results, key=lambda x: x[1])}
     return(results)
-    
+
+
 def generate_markdown_table_mainside(link_dictionary, title):
     res = StringIO()
     writer = pytablewriter.MarkdownTableWriter()
     writer.stream = res
-    writer.table_name=title
+    writer.table_name = title
     summation = {}
     for cur in link_dictionary:
         nums = [x.split(" ")[0] for x in link_dictionary[cur]]
         summation[cur] = sum(int(x) for x in nums)
-    writer.header_list = list([f"{x} ({summation[x]})" for x in link_dictionary.keys()])
+    writer.header_list = list(
+        [f"{x} ({summation[x]})" for x in link_dictionary.keys()])
     writer.value_matrix = list(zip_longest(*list(link_dictionary.values())))
     writer.write_table()
     return res
-    
+
+
 def generate_markdown_table_options(input, title):
     maindeck, sideboard = input
     if not maindeck and not sideboard:
         return None
     nmd, md = [x[0] for x in maindeck], [x[1] for x in maindeck]
-    nsb, sb= [x[0] for x in sideboard], [x[1] for x in sideboard]
+    nsb, sb = [x[0] for x in sideboard], [x[1] for x in sideboard]
     res = StringIO()
     writer = pytablewriter.MarkdownTableWriter()
     writer.stream = res
-    writer.table_name=title
-    writer.header_list = ["*n*<sup>th</sup> copy", "Maindeck Card", "*n*<sup>th</sup> copy", "Sideboard Card"]
+    writer.table_name = title
+    writer.header_list = ["*n*<sup>th</sup> copy",
+                          "Maindeck Card", "*n*<sup>th</sup> copy", "Sideboard Card"]
     writer.value_matrix = list(zip_longest(nmd, md, nsb, sb))
     writer.write_table()
     return res
-    
+
+
 def merge_markdown_tables(input1, input2, title):
     res = StringIO()
     writer = pytablewriter.MarkdownTableWriter()
     writer.stream = res
-    writer.table_name=title
+    writer.table_name = title
     writer.header_list = ["Maindeck Options", "Sideboard Options"]
     writer.value_matrix = [[input1, input2]]
     writer.write_table()
     return res
-    
+
+
 def run(title, dir, format):
     everything = f"# {title}\n\n#### [Download MTGO](../collection/{title.replace(' ', '%20')}/{title.replace(' ', '%20')}.txt)"
     for cur in ["Maindeck", "Sideboard"]:
@@ -108,35 +118,34 @@ def run(title, dir, format):
         a = get_links_group_by_types(inputs)
         q = generate_markdown_table_mainside(a, f"{cur}\n")
         everything += "\n" + q.getvalue()
-    
+
     temp = []
     for cur in ["Maindeck", "Sideboard"]:
         with open(os.path.join(dir, f"{cur}_options.txt")) as fp:
             inputs = fp.readlines()
         inputs = [x.strip() for x in inputs]
         temp.append([return_url_line_type(x)[0].split(" ", 1) for x in inputs])
-    
+
     q = generate_markdown_table_options(temp, "Other Options\n")
     if q:
         everything += "\n" + q.getvalue()
-    
+
     if not exists(f"./{format}/decks"):
         makedirs(f"./{format}/decks")
     with open(f"./{format}/decks/{title.replace(' ', '_')}.md", "w") as fp:
         fp.write(everything)
 
+
 if __name__ == "__main__":
     start = time()
     for format in ["Standard", "Modern", "Legacy"]:
         d = f'./{format}/collection'
-        archetypes = [(o, os.path.join(d, o)) for o in os.listdir(d) 
-                        if os.path.isdir(os.path.join(d,o))]
+        archetypes = [(o, os.path.join(d, o)) for o in os.listdir(d)
+                      if os.path.isdir(os.path.join(d, o))]
         for title, dir in archetypes:
             print(f"Working on {title}")
             run(title, dir, format)
-        
+
         with open("card_backup.pkl", "wb") as fp:
             pickle.dump(memoizer, fp)
     print("Time to completion:", str(datetime.timedelta(seconds=time()-start)))
-    
-    
--- generate_readme.py
+++ generate_readme.py
@@ -6,22 +6,26 @@ from itertools import zip_longest
 from math import ceil
 import datetime
 
+
 def format_paths(format):
     mypath = f"./{format}/decks/"
-    onlyfiles = sorted([f"[{f[:-3].replace('_', ' ')}]({mypath}{f})" for f in listdir(mypath) if isfile(join(mypath, f))])
+    onlyfiles = sorted(
+        [f"[{f[:-3].replace('_', ' ')}]({mypath}{f})" for f in listdir(mypath) if isfile(join(mypath, f))])
     return onlyfiles
-    
+
+
 def chunks(l, n):
     """Yield successive n-sized chunks from l."""
     for i in range(0, len(l), n):
         yield l[i:i + n]
-    
+
+
 def generate_table(title, files):
     res = StringIO()
     writer = pytablewriter.MarkdownTableWriter()
     writer.stream = res
-    writer.table_name=title
-    #writer.header_list = #list(link_dictionary.keys())
+    writer.table_name = title
+    # writer.header_list = #list(link_dictionary.keys())
     val = 1
     while (ceil(len(files) / val) != 5):
         val += 1
@@ -29,20 +33,23 @@ def generate_table(title, files):
     writer.value_matrix = list(zip_longest(*list(chunks(files, val))))
     writer.write_table()
     return res
-    
+
+
 def create(format):
     files = format_paths(format)
     q = generate_table(f"{format}\n", files)
     return q.getvalue()
-    
+
+
 def output():
     everything = "# Stock Decklists\n\n"
     for format in ["Standard", "Modern", "Legacy"]:
         everything += "\n" + create(format)
-        
+
     everything += f"\n\n#### Last Updated at {datetime.datetime.now().strftime('%I:%M%p on %B %d, %Y')}"
     with open("README.md", "w") as fp:
         fp.write(everything)
-    
+
+
 if __name__ == "__main__":
     output()
