9092742856c85607199c8a68ed8e76b37b96542b
Joshua Gang <joshua.gang@gmail.com>
2018-07-30 13:30:50 -0400

    Moved a rebuild of the cache to cleanup.py, this should take care of most disappeared temporary files. Still leaving in code in scraper.py in order to ensure that temp file doesn't disappear between the two scripts running.

--- cleanup.py
+++ cleanup.py
@@ -1,5 +1,6 @@
 from shutil import rmtree
 from os.path import exists
+import pickle
 
 if exists("./Modern"):
     rmtree("./Modern")
@@ -7,3 +8,10 @@ if exists("./Legacy"):
     rmtree("./Legacy")
 if exists("./Standard"):
     rmtree("./Standard")
+if exists("cached_files.pkl"):
+    with open("cached_decklists.pkl") as fp:
+        cached_files = pickle.load(fp)
+    cached_files = {k:v for k,v in cached_files.items() if exists(v)}
+    with open("cached_decklists.pkl", "wb") as fp:
+        pickle.dump(cached_files, fp)
+    
